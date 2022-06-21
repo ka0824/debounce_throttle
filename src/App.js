6d3b7128ac4b0b5018fdcbd4e3992aa87afe93ec
KaHyung Kim <rkgud0824.r2ware.com>
2022-06-21 12:36:04 +0900

    feat: make debounce, throttle template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import DisplayProps from "./DisplayProps";
+import debounce from "lodash/debounce";
+import { throttle } from "lodash";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const debounceInputValue = useCallback(debounce(setInputValue, 300), []);
+  const throttleInputValue = useCallback(throttle(setInputValue, 1000), []);
+
+  const handleInput = (e) => {
+    // debounceInputValue(e.target.value);
+    throttleInputValue(e.target.value);
+    // throttle(setInputValue(e.target.value), 500);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        // onChange={(e) => setInputValue(e.target.value)}
+        onChange={handleInput}
+      ></input>
+      <DisplayProps inputValue={inputValue}></DisplayProps>
     </div>
   );
 }
---
+++ src/DisplayProps.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DisplayProps = ({ inputValue }) => {
+  return (
+    <div>
+      <div>현재 inputValue는 {inputValue}입니다.</div>
+      <div>렌더링이 되나 테스트 합니다.2</div>
+      <div>렌더링이 되나 테스트 합니다.3</div>
+      <div>렌더링이 되나 테스트 합니다.4</div>
+      <div>렌더링이 되나 테스트 합니다.5</div>
+    </div>
+  );
+};
+
+export default DisplayProps;
